f86855ab2165773951316c199ec754a1f7a62a6b
kjmatt91 <kjmatt91@gmail.com>
2021-09-21 22:11:53 -0400

    finished setting up the table creation migration file

--- migrations/20210915022244-intial-table-creation.js
+++ migrations/20210915022244-intial-table-creation.js
@@ -3,30 +3,60 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.createTable('authors', {
-      id: {
-        type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true
-      },
-      author: {
-        type: Sequelize.STRING, allowNull: false
-      },
-      createdAt: {
-        type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
-      },
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      author: { type: Sequelize.STRING, allowNull: false },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
       updatedAt: {
-        type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT-TIMESTAMP')
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
       },
-      deletedAt: {
-        type: Sequelize.DATE
+      deletedAt: { type: Sequelize.DATE, },
+    })
+
+    await queryInterface.createTable('genres', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      genre: { type: Sequelize.STRING, allowNull: false },
+      createdAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'), },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
       },
+      deletedAt: { type: Sequelize.DATE, },
+    })
+
+    await queryInterface.createTable('titles', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      title: { type: Sequelize.STRING, allowNull: false },
+      authorId: { type: Sequelize.INTEGER, references: { model: 'authors', key: 'id' } },
+      createdAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'), },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+      deletedAt: { type: Sequelize.DATE, },
+    })
+
+    return queryInterface.createTable('titlesGenres', {
+      genreId: { type: Sequelize.INTEGER, references: { model: 'genres', key: 'id' } },
+      titleId: { type: Sequelize.INTEGER, references: { model: 'titles', key: 'id' } },
+      createdAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'), },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'),
+      },
+      deletedAt: { type: Sequelize.DATE, },
     })
   },
 
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('titlesGenres')
+    await queryInterface.dropTable('titles')
+    await queryInterface.dropTable('genres')
+
+    return queryInterface.dropTable('authors')
   }
-};
+}
